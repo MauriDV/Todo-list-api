eb99a76c87f980b7eb65731dc81b420d0cb660d7
MauriDV <mdellevedove12@gmail.com>
2017-05-05 02:17:28 -0300

    db schema

---
+++ schema/schema.sql
@@ -0,0 +1,21 @@
+drop database if exists todolist;
+
+create database if not exists todolist;
+
+use todolist;
+
+drop table if exists user;
+
+create table if not exists user(
+   userId integer not null primary key auto_increment,
+   username varchar(100) unique,
+   password varchar(100),
+)engine=innodb;
+
+create table if not exists task(
+   taskId integer primary key auto_increment,
+   userTask integer not null,
+   taskText varchar(100) unique,
+   completed boolean,
+   constraint fk_user foreign key (userTask) references user(userId) on delete cascade
+)engine=innodb;
