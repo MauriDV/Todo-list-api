e5476dd8f3b9ae517cef480fafea0cbefffa37a5
MauriDV <mdellevedove12@gmail.com>
2017-05-05 02:40:41 -0300

    task model

---
+++ models/task.js
@@ -0,0 +1,16 @@
+/*
+
+Task Model
+
+*/
+
+class Task {
+  constructor(user,content,completed){
+    this.user = user; //owner of task
+    this.content = content; //content of task
+    this.completed = completed; //true if task is completed else false
+  }
+}
+
+//Export Task
+exports.model = Task;
