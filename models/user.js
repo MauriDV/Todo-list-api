a21acf13c87fe484177b93e700e7a71b3990d674
MauriDV <mdellevedove12@gmail.com>
2017-05-05 02:29:20 -0300

    add user model

---
+++ models/user.js
@@ -0,0 +1,15 @@
+/*
+
+User Model
+
+*/
+
+class User {
+  constructor(username,password){
+    this.username = username;
+    this.password = password;
+  }
+}
+
+//Export User
+module.exports = User;
